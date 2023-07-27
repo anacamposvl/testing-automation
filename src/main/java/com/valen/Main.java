25c3ee52fd20be387a95e786bd18b15a33ddf477
anacamposvl <anacamposlozada@gmail.com>
2023-07-26 23:29:17 -0300

    Proyecto base

---
+++ src/main/java/com/valen/Main.java
@@ -0,0 +1,10 @@
+package com.valen;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Testing Automation\n");
+        System.out.printf("Adios");
+
+
+        }
+    }
