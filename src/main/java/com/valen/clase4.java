c52fdd0663488f50d1d210f575ac7941da8955dd
anacamposvl <anacamposlozada@gmail.com>
2023-08-09 18:26:12 -0300

    agregue clase4

--- src/main/java/com/valen/clase4.java
+++ src/main/java/com/valen/clase4.java
@@ -1,2 +1,12 @@
-package com.valen;public class clase4 {
+package com.valen;
+
+public class clase4 {
+
+    public static void main(String[] args){
+    Estudiante pepe = new Estudiante();
+    pepe.redirExamen("programacion java");
+
+    pepe.setNombre("pepe");
+    System.out.println(" el nombre del destudiante es: " + pepe.getNombre());
+    }
 }
